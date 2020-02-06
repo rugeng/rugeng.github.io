80d3e4aca9ec506895a0e074f771f378d62fa41b
RU GENG <59707636+rugeng@users.noreply.github.com>
2020-02-06 10:57:02 -0800

    Add files via upload

--- des157-wi20/js/script.js
+++ des157-wi20/js/script.js
@@ -18,6 +18,6 @@ function draw() {
 }
 function mousePressed() {
     background(random(200, 255));
-    fill(random(0,255),random(0,255),random(0,255));
+    fill(0, 100, 100, 64);
     ellipse(mouseX, mouseY, 30, 30);
 }
