fe4d5f2d08aa709e310754c540ded8f4c0d1d6be
RU GENG <59707636+rugeng@users.noreply.github.com>
2020-02-06 10:48:13 -0800

    Add files via upload

---
+++ des157-wi20/js/script.js
@@ -0,0 +1,23 @@
+
+function setup() {
+    
+    var myCanvas =  createCanvas(800, 250);
+    noStroke();
+    background(200);
+    myCanvas.parent(mySketch);
+}
+
+function draw() {
+    fill(0, 0, 100, 64);
+    var brushSize = 20;
+    if (mouseIsPressed) {
+    rect(mouseX, mouseY, brushSize, brushSize);
+    }else {
+    ellipse(mouseX, mouseY, brushSize, brushSize);
+    }
+}
+function mousePressed() {
+    background(random(200, 255));
+    fill(0, 100, 100, 64);
+    ellipse(mouseX, mouseY, brushSize*2, brushSize*2);
+}
